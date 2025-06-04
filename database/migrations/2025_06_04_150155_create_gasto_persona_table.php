443a52cda2bae7b97b702cb0e3564ffcb30df88b
Noah Galvez <ngalvez@factoriabiz.com>
2025-06-04 17:05:36 +0200

    Migraciones

---
+++ database/migrations/2025_06_04_150155_create_gasto_persona_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('gasto_persona', function (Blueprint $table) {
+            $table->foreignId('gasto_id')->constrained()->onDelete('cascade');
+            $table->foreignId('persona_id')->constrained()->onDelete('cascade');
+            $table->decimal('cantidad', 10, 2)->default(0);
+            $table->primary(['gasto_id', 'persona_id']);
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gasto_persona');
+    }
+};
